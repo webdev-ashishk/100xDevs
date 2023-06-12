f310501f7cffce6f6cf162a56b90f50655a2a26e
webdev-ashishk <webdev.ashishk@gmail.com>
2023-06-12 10:21:47 +0530

    Week-1-assignment(hard)/promisify-settimeout.js called

--- Week-1/Week-1-assignment/02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ Week-1/Week-1-assignment/02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -1,16 +1,29 @@
 /*
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
+/*
+Sure! Here's an explanation of how to write such a function in simple English:
 
-function wait(n) {
-  return new Promise(function (resolves, rejects) {
+To write a function that returns a promise, which is a special object in JavaScript used for asynchronous operations, you can follow these steps:
+
+1. Define the function and give it a name, let's say it's called "delay".
+2. The function should take a single argument, let's call it "seconds", which represents the number of seconds to wait before resolving the promise.
+3. Inside the function, create a new promise using the Promise constructor. The constructor takes a callback function as an argument.
+4. Within the callback function, use the setTimeout() function to delay the resolution of the promise. The setTimeout() function takes a callback function and a time in milliseconds.
+5. Convert the "seconds" argument to milliseconds by multiplying it by 1000 (since there are 1000 milliseconds in a second).
+6. Pass the callback function to setTimeout() and set the delay time to the converted milliseconds.
+7. In the callback function, call the resolve() function to indicate that the promise has been successfully resolved after the specified delay.
+8. Return the promise object from the function.
+
+By following these steps, you will have a function named "delay" that returns a promise. When you call this function with a specific number of seconds, it will create a promise that resolves after that many seconds have passed.
+*/
+function delay(second) {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      console.log("setTimeout-called-1");
-      resolves();
-    });
-  }, n);
+      resolve();
+    }, second * 1000);
+  });
 }
-function n() {
-  console.log("n is called !");
-}
-wait().then(n);
+delay(5).then(() => {
+  console.log("promise resolved after 5 seconds");
+});
