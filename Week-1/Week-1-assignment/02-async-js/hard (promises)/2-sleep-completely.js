340bf8561f9269874742938c62286ed51abe9aef
webdev-ashishk <webdev.ashishk@gmail.com>
2023-06-12 14:38:03 +0530

    Week-1-assignment(hard)/2.sleep complately

--- Week-1/Week-1-assignment/02-async-js/hard (promises)/2-sleep-completely.js
+++ Week-1/Week-1-assignment/02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,14 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(n) {
+  for (let i = 0; i <= n; i++) {
+    console.log("busy");
+  }
 }
+sleep(1000000000);
+console.log("a");
+console.log("b");
+console.log("c");
+console.log("d");
+console.log("e");
